8bbceb082672cf8b8d6328e30a18d88b32c331e9
Aaron Arney <aaron.g.arney@gmail.com>
2016-08-02 20:30:44 -0400

    Add redux example

--- app/components/redux-todo-example.jsx
+++ app/components/redux-todo-example.jsx
@@ -9,9 +9,38 @@ var stateDefault = {
 };
 
 var reducer = (state = stateDefault, action) => {
-  return state;
+
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+      break;
+    default:
+      return state;
+  }
 };
-var store = redux.createStore(reducer);
+
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+  console.log('Name is', state.name);
+});
+// unsubscribe();
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Aaron'
+});
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Kristin'
+});
